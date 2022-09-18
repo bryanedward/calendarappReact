28ab6f0ed35198237426de2cf00e5e4c4e008fdb
edward <edwardbrian96@gmail.com>
2022-09-18 10:57:41 -0500

    validar url

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import { AppRouter } from './Routers/AppRouter'
-import { store } from './store/store'
+import React from "react";
+// import { Provider } from 'react-redux'
+// import { AppRouter } from './Routers/AppRouter'
+// import { store } from './store/store'
 
 export function App() {
   return (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
-  )
+    // <Provider store={store}>
+    //   <AppRouter />
+    // </Provider>
+    <div>{process.env.REACT_APP_API_URL}</div>
+  );
 }
